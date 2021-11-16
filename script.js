51d8def1560942641caa8a8e38a72eda88443d3c
Justin <kaytin143@gmail.com>
2021-11-16 10:46:01 -0700

    defined library outside function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+
 
 // This is my Book Contructor! //
 function Book(title,author,pages) {
@@ -6,29 +6,27 @@ this.title=title;
 this.author=author;
 this.pages=pages;
 }
-
 const addBook = (ev) => {
     ev.preventDefault();
-
-
+    let myLibrary = [];
 let bookInfo = {
     title: document.getElementById('title').value,
     author: document.getElementById('author').value,
-    pages: document.getElementById('pages').value,
+    pages: document.getElementById('pages').value
 }
-myLibrary.push(bookInfo)
-// reset forms after submitting //
-
+myLibrary.push(bookInfo);
 for (i = 0; i < myLibrary.length; i++) {
-    console.log(myLibrary)
+    console.log(myLibrary[i])
     var container = document.getElementById('book-shelf')
      var div = document.createElement('div')
      div.classList.add('cell')
      container.appendChild(div);
 }
-
-
+// reset forms after submitting //
+document.forms[0].reset();
 }
+
+
 function addBtn() {
     var form = document.getElementById('myForm');
     form.style.display = 'flex' ;
@@ -39,6 +37,5 @@ function closeBtn() {
 }
 var submitBtn = document.querySelector('.submit-btn');
 submitBtn.addEventListener('click',addBook)
-
 
 // Book-shelf cards //
