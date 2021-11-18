a4386cb7aec0b94f3947a42a027831e07b15c19e
Justin <kaytin143@gmail.com>
2021-11-18 02:06:47 -0700

    Fixed multiple divs appearing

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let myLibrary = [];
-
+myLibrary = [];
 
 //Constructor//
 function Book(title,author,pages,) {
@@ -8,9 +7,7 @@ function Book(title,author,pages,) {
     this.pages = pages;
 }
 
-
 function addBookToLibrary() {
-
     let bookInfo = {
     title: document.getElementById('title').value,
     author: document.getElementById('author').value,
@@ -18,12 +15,43 @@ function addBookToLibrary() {
 }
 myLibrary.push(bookInfo)
 
+myLibrary.forEach(book =>  {
+    console.log(myLibrary)
+});
+
+
+     var container = document.getElementById('book-shelf')
+     var div = document.createElement('div')
+     var divT = document.createElement('p')
+     var divA = document.createElement('p')
+     var divP = document.createElement('p')
+     var divRead = document.createElement('button')
+     var divX = document.createElement('button')
+     var checkBox = document.getElementById('checkBox')
+     div.classList.add('cell')
+     divRead.classList.add('readBox')
+     divX.classList.add('remove-book')
+     divT.classList.add('title')
+     divT.innerHTML = `${title.value}`
+     divA.innerHTML = `${author.value}`
+     divP.innerHTML = `${pages.value}`
+     divX.innerHTML = '×'
+     divX.dataset.name = `${title.value}`
+     div.dataset.name = `${title.value}`
+     container.appendChild(div);
+     div.appendChild(divT)
+     div.appendChild(divA)
+     div.appendChild(divP)
+     div.appendChild(divRead)
+     div.appendChild(divX)
+     if (checkBox.checked == true) {
+        divRead.innerHTML = 'Already Read'
+    } else {
+        divRead.innerHTML = 'Not read yet'
+    }
 
 }
-function test() {
-    myLibrary.forEach(function(i){
-          console.log(i);
-        })}
+
 // add button to open forms //
 function addBtn() {
     var addBtn = document.getElementById('myForm')
@@ -35,4 +63,4 @@ var closeBtn = document.querySelector('.x');
 closeBtn.style.display=none;
 console.log('should work')
 }
-
+var readBtn = document.getElementById('readBtn')
