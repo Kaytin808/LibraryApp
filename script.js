d4d00c09825b0a4f36ce2c1717d6c1b1903cf54a
Justin Moore <kaytin143@gmail.com>
2021-11-28 17:48:32 -0700

    Fixed chck box on read/notread

--- script.js
+++ script.js
@@ -24,34 +24,15 @@ function displayBooks(book) {
     divT.classList.add('title')
     var divA = document.createElement('p')
     var divP = document.createElement('p')
-    var btnContainer = document.createElement('div')
-    btnContainer.classList.add('btn-container')
     var divRead = document.createElement('button')
     divRead.classList.add('readBox')
-    var divX = document.createElement('button')
-    divX.setAttribute('data-index-number',myLibrary.indexOf(book))
-
+    var divX = document.createElement('button')   
+    var checkBox = document.getElementById('checkBox')
     divX.classList.add('remove-book')
     divT.innerHTML = `${title.value}`
     divA.innerHTML = `${author.value}`
     divP.innerHTML = `${pages.value}` + ' pages'
     divX.innerHTML = '×'
-    bookShelf.appendChild(bookCell)
-    bookCell.appendChild(divT)
-    bookCell.appendChild(divA)
-    bookCell.appendChild(divP)
-    bookCell.appendChild(divRead)
-    bookCell.appendChild(divX)
-    bookCell.appendChild(btnContainer)
-    // Form reset after pressing submit //
-    document.forms[0].reset();
-
-divX.onclick = function(e) {
-    var itemIndex = this.getAttribute('data-index-number')
-    myLibrary.splice(itemIndex,1)
-    e.target.parentElement.remove()
-    console.log(myLibrary)
-}
     if (checkBox.checked == true) {
         divRead.innerHTML = 'Already read'
     } else {
@@ -64,52 +45,24 @@ divX.onclick = function(e) {
             divRead.innerHTML= 'Already read'
         }
     })
-console.log(myLibrary)
-     var container = document.getElementById('book-shelf')
-     var div = document.createElement('div')
-     var divT = document.createElement('p')
-     var divA = document.createElement('p')
-     var divP = document.createElement('p')
-     var divRead = document.createElement('button')
-     var divX = document.createElement('button')
-     var checkBox = document.getElementById('checkBox')
-     div.classList.add('cell')
-     divRead.classList.add('readBox')
-     divX.classList.add('remove-book')
-     divT.classList.add('title')
-     divT.innerHTML = `${title.value}`
-     divA.innerHTML = `${author.value}`
-     divP.innerHTML = `${pages.value}`
-     divX.innerHTML = '×'
-     divX.dataset.name = `${title.value}`
-     div.dataset.name = `${title.value}`
-     container.appendChild(div);
-     div.appendChild(divT)
-     div.appendChild(divA)
-     div.appendChild(divP)
-     div.appendChild(divRead)
-     div.appendChild(divX)
-     if (checkBox.checked == true) {
-        divRead.innerHTML = 'Already Read'
-    } else {
-        divRead.innerHTML = 'Not read yet'
+
+    bookShelf.appendChild(bookCell)
+    bookCell.appendChild(divT)
+    bookCell.appendChild(divA)
+    bookCell.appendChild(divP)
+    bookCell.appendChild(divRead)
+    bookCell.appendChild(divX)
+    // Form reset after pressing submit //
+    document.forms[0].reset();
+
+    // remove from array on button //
+    divX.onclick = function(e) {
+        myLibrary.splice(myLibrary.indexOf(book),1)
+        e.target.parentElement.remove()
+        console.log(myLibrary)
     }
-    divRead.addEventListener('click',() => {
-        if (divRead.innerHTML == 'Already Read') {
-            divRead.innerHTML = 'Not read yet'
-        } else {
-            divRead.innerHTML = 'Already Read'
-        }
-    })
-    var removeBtn = document.querySelectorAll('.remove-book')
-    removeBtn.forEach(books => {
-        books.addEventListener('click',() => {
-    if (divX.dataset.name === div.dataset.name)
-    div.remove(div)
-    })
-    })
-    
 }
+console.log(myLibrary)
 
 // add/close button to open forms //
 function addBtn() {
