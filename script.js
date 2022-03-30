f15cc3510e411662eeb4349321a4b2640005f93c
Justin <kaytin143@gmail.com>
2022-03-29 19:12:54 -0700

    Fixed merge conflicts

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
  let myLibrary = [];
 
 //Constructor//
-<<<<<<< HEAD
 class Book {
     constructor(title,author,pages) {
         this.title = title;
         this.author = author;
         this.pages = pages;
     }
-=======
-function Book(title,author,pages,) {
-    this.title = title.value;
-    this.author = author.value;
-    this.pages = pages.value;
->>>>>>> 54349327f774f3e164279a34dbc69ae57aa16a96
 }
 
 function addBookToLibrary(item) {
     var form = document.getElementById('myForm')
     form.style.display='none';
-<<<<<<< HEAD
 
     let bookInfo = {
     title: document.getElementById('title').value,
@@ -63,33 +55,6 @@ function addBook() {
      div.appendChild(divX)
      if (checkBox.checked == true) {
         divRead.innerHTML = 'Already Read'
-=======
-
-    const book = new Book(title,author,pages)
-    myLibrary.push(book)
-    displayBooks(book)
-    console.log(myLibrary)
-}
-function displayBooks(book) {
-    var bookShelf = document.getElementById('book-shelf')
-    var bookCell = document.createElement('div')
-    bookCell.classList.add('cell')
-    var divT = document.createElement('p')
-    divT.classList.add('title')
-    var divA = document.createElement('p')
-    var divP = document.createElement('p')
-    var divRead = document.createElement('button')
-    divRead.classList.add('readBox')
-    var divX = document.createElement('button')   
-    var checkBox = document.getElementById('checkBox')
-    divX.classList.add('remove-book')
-    divT.innerHTML = `${title.value}`
-    divA.innerHTML = `${author.value}`
-    divP.innerHTML = `${pages.value}` + ' pages'
-    divX.innerHTML = '×'
-    if (checkBox.checked == true) {
-        divRead.innerHTML = 'Already read'
->>>>>>> 54349327f774f3e164279a34dbc69ae57aa16a96
     } else {
         divRead.innerHTML = 'Not read yet'
     }
@@ -100,7 +65,6 @@ function displayBooks(book) {
             divRead.innerHTML= 'Already read'
         }
     })
-<<<<<<< HEAD
     var removeBtn = document.querySelectorAll('.remove-book')
     removeBtn.forEach(books => {
     books.addEventListener('click', (e)=> {
@@ -114,28 +78,6 @@ function displayBooks(book) {
 // reset forms after submit //
 document.forms[0].reset();
 // add button to open forms //
-=======
-
-    bookShelf.appendChild(bookCell)
-    bookCell.appendChild(divT)
-    bookCell.appendChild(divA)
-    bookCell.appendChild(divP)
-    bookCell.appendChild(divRead)
-    bookCell.appendChild(divX)
-    // Form reset after pressing submit //
-    document.forms[0].reset();
-
-    // remove from array on button //
-    divX.onclick = function(e) {
-        myLibrary.splice(myLibrary.indexOf(book),1)
-        e.target.parentElement.remove()
-        console.log(myLibrary)
-    }
-}
-console.log(myLibrary)
-
-// add/close button to open forms //
->>>>>>> 54349327f774f3e164279a34dbc69ae57aa16a96
 function addBtn() {
     var addBtn = document.getElementById('myForm')
     addBtn.style.display = 'flex';
@@ -144,8 +86,4 @@ function closeBtn() {
     var form = document.getElementById('myForm')
 form.style.display='none';
 }
-<<<<<<< HEAD
 var readBtn = document.getElementById('readBtn')
-=======
-    var readBtn = document.getElementById('readBtn')
->>>>>>> 54349327f774f3e164279a34dbc69ae57aa16a96
